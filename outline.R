149b8e131b5329f54601dc9da46895a00b3151b7
danieleum9 <seum@uw.edu>
2022-05-30 16:15:16 -0700

    basic things added

--- outline.R
+++ outline.R
@@ -1,1 +1,17 @@
+#Introductory Page, scss - Leo
 
+
+
+#Page 1 - Select country function graph - Daniel E
+
+
+
+#Page 2 - select year for top 3 countries graph - Finn
+
+
+
+#Page 3 - choreography - Daniel M
+
+
+
+#Conclusion, theme - Rupali
--- ui.R
+++ ui.R
@@ -1,3 +1,4 @@
 library(plotly)
 library(bslib)
 
+air_df <- read.csv("PM2.5_Global_Air_Pollution_2010-2017.csv", stringsAsFactors = FALSE)
