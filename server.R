60c9454c0ba67616ae0e5c1d7383ed72cc14a302
danieleum9 <seum@uw.edu>
2022-05-30 16:01:44 -0700

    df

--- server.R
+++ server.R
@@ -1,3 +1,6 @@
 library(ggplot2)
 library(plotly)
 library(dplyr)
+
+air_df <- read.csv("PM2.5_Global_Air_Pollution_2010-2017.csv", stringsAsFactors = FALSE)
+
--- ui.R
+++ ui.R
@@ -1,2 +1,3 @@
 library(plotly)
 library(bslib)
+
