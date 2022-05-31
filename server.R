6a72abe4e70c53b2bd358c452e906eedf367a36b
danieleum9 <seum@uw.edu>
2022-05-31 15:55:50 -0700

    daniele work added

--- server.R
+++ server.R
@@ -1,6 +1,35 @@
 library(ggplot2)
 library(plotly)
 library(dplyr)
+library("tidyverse")
 
 air_df <- read.csv("PM2.5_Global_Air_Pollution_2010-2017.csv", stringsAsFactors = FALSE)
 
+server <- function(input, output) {
+  
+  output$plot1 <- renderPlotly({
+
+    edit_df <- air_df %>% 
+      pivot_longer(!c(Country.Name, Country.Code), 
+                   names_to = "Year",
+                   values_to = "PM2.5")
+    
+    filtered_df <- edit_df %>% 
+      filter(Country.Name %in% input$user_selection)
+    
+    air_plot1 <- ggplot(data = filtered_df) +
+      geom_col(mapping = aes(x = Year, y = PM2.5, fill = Country.Name)) + labs(
+        title = "Annual Mean PM2.5 Exposure in Each Country 2010 - 2017",
+        x = "Year",
+        y = "Mean PM2.5 Exposure (Micrograms per Cubic Meter)",
+        fill = "Country Name"
+      )
+      
+    
+    ggplotly(air_plot1)
+    
+  })
+}
+    
+    
+      
--- ui.R
+++ ui.R
@@ -2,3 +2,39 @@ library(plotly)
 library(bslib)
 
 air_df <- read.csv("PM2.5_Global_Air_Pollution_2010-2017.csv", stringsAsFactors = FALSE)
+
+intro_tab <- tabPanel(
+  "Introduction",
+  fluidPage(
+    p("")
+  )
+)
+
+sidebar_panel_widget <- sidebarPanel(
+  selectInput(
+    inputId = "user_selection",
+    label = "YOUR CODE HERE",
+    choices = air_df$Country.Name,
+    multiple = FALSE
+  )
+)
+
+
+main_panel_plot <- mainPanel(
+  plotlyOutput(outputId = "plot1")
+)
+
+climate_tab <- tabPanel(
+  "PM 2.5",
+  sidebarLayout(
+    sidebar_panel_widget,
+    main_panel_plot
+  )
+)
+
+
+ui <- navbarPage(
+  "PM2.5 Levels",
+  intro_tab,
+  climate_tab
+)
