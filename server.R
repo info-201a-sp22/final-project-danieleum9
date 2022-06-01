bebdbd9f3ba731485f9d402e91d8453999398196
dmarti509 <dmarti4@uw.edu>
2022-05-31 20:43:47 -0700

    plot 2 done

--- server.R
+++ server.R
@@ -2,6 +2,10 @@ library(ggplot2)
 library(plotly)
 library(dplyr)
 library("tidyverse")
+library(choroplethrMaps)
+
+data(country.map, package = "choroplethrMaps")
+
 
 air_df <- read.csv("PM2.5_Global_Air_Pollution_2010-2017.csv", stringsAsFactors = FALSE)
 
@@ -30,7 +34,55 @@ server <- function(input, output) {
     ggplotly(air_plot1, tooltip = "text")
     
   })
+  output$plot2 <- renderPlotly({ 
+    
+    choropleth_data_df <- Pollution_df %>% 
+      pivot_longer(!c(`Country Name`, `Country Code`), 
+                   names_to = "Year",
+                   values_to = "PM2.5")
+    
+    filtered_map <- choropleth_data_df %>% 
+      filter(Year %in% input$user_selection)
+    
+    air_map <- choropleth_data_df %>% 
+      filter(Year %in% input$Year_selection) %>% 
+      rename(region = `Country Name`, value = PM2.5) %>% 
+      mutate(region = tolower(region)) %>% 
+      mutate(region = recode(
+        region,
+        "united states"             = "united states of america",
+        "congo, dem. rep."          = "democratic republic of the congo",
+        "congo, rep."               = "republic of congo",
+        "korea, dem. rep."          = "south korea",
+        "korea. rep."               = "north korea",
+        "tanzania"                  = "united republic of tanzania",
+        "serbia"                    = "republic of serbia",
+        "slovak republic"           = "slovakia",
+        "yemen, rep."               = "yemen", 
+        "russian federation"        = "russia",
+        "north macedonia"           = "macedonia", 
+        "korea, dem. people's rep." = "north korea", 
+        "korea, rep."               = "south korea", 
+        "venezuela, rb"             = "venezuela", 
+        "iran, islamic rep."        = "iran", 
+        "lao pdr"                   = "laos", 
+        "syrian arab republic"      = "syria",
+        "bahamas, the"              = "the bahamas", 
+        "timor-leste"               = "east timor", 
+        "brunei darussalam"         = "brunei", 
+        "cote d'ivoire"             = "ivory coast", 
+        "northern cyprus"           = "cyprus", 
+        "gambia, the"               = "gambia", 
+        "guinea-bissau"             = "guinea bissau", 
+        "egypt, arab rep."          = "egypt", 
+        "kyrgyz republic"           = "kyrgyzstan",
+        "eswatini"                  = "swaziland")) 
+
+    
+    choropleth_air<- country_choropleth(air_map) +
+      labs(title = "Annual Mean PM2.5 Exposure in Each Country Map 2010 - 2017")
+    
+    ggplotly(choropleth_air)
+    
+  })
 }
-    
-    
-      
